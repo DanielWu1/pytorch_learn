125b7d0a98698bd4d84d34d1205bde57711892d5
DanielWu1 <Tony153265964@gmail.com>
2021-10-27 17:45:43 -0400

    15-17

---
+++ dataloader.py
@@ -0,0 +1,25 @@
+import torchvision
+
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+test_data = torchvision.datasets.CIFAR10('./dataset', train =False, transform = torchvision.transforms.ToTensor())
+
+test_loader = DataLoader(dataset = test_data, batch_size= 64, shuffle=False, num_workers=0, drop_last = True)
+
+#测试数据集中第一张图片target
+
+img, target = test_data[0]
+
+writer = SummaryWriter("dataloader")
+
+for epoch in range(2):
+    step = 0
+    for data in test_loader:
+        imgs, targets = data
+        # print(imgs.shape)
+        # print(targets)
+        writer.add_images('test_data', imgs, step)
+        step += 1
+
+writer.close()
