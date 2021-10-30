cde266465f3d98295e7f097a08316a7c7961a2d3
DanielWu1 <Tony153265964@gmail.com>
2021-10-30 01:35:45 -0400

    P24

---
+++ nn_optim.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+from torch.nn import ReLU, Sigmoid
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential, CrossEntropyLoss
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter, writer
+
+
+dataset = torchvision.datasets.CIFAR10('./dataset', train =False, transform = torchvision.transforms.ToTensor(), download = True)
+dataloader = DataLoader(dataset, batch_size=64)
+
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        # self.conv1 = Conv2d(3, 32, 5, padding=2)
+        # self.maxpool1 = MaxPool2d(2)
+        # self.conv2 = Conv2d(32, 32, 5, padding=2)
+        # self.maxpool2 = MaxPool2d(2)
+        # self.conv3 = Conv2d(32, 64, 5, padding=2)
+        # self.maxpool2 = MaxPool2d(2)
+        # self.flatten = Flatten()
+        # self.linear1 = Linear(1024, 64)
+        # self.linear2 = Linear(64, 10)
+
+
+        self.modle1 = Sequential(
+            Conv2d(3, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+
+    def forward(self, x):
+        # x = self.conv1(x)
+        # x = self.maxpool1(x)
+        # x = self.conv2(x)
+        # x = self.maxpool2(x)
+        # x = self.conv3(x)
+        # x = self.maxpool2(x)
+        # x = self.flatten(x)
+        # x = self.linear1(x)
+        # x = self.linear2(x)
+
+        x = self.modle1(x)
+        return x
+
+loss = CrossEntropyLoss()
+tudui = Tudui()
+optim = torch.optim.SGD(tudui.parameters(), lr= 0.01)
+
+for epoch in range(20):
+    running_loss= 0.0
+    for data in dataloader:
+        imgs, targets = data
+        outputs = tudui(imgs)
+        result_loss = loss(outputs, targets)
+        optim.zero_grad()
+        # print(result_loss)
+        result_loss.backward()
+        optim.step()
+        running_loss = running_loss + result_loss
+
+    print(running_loss)
+# input = torch.ones(64, 3, 32, 32)
+# output = tudui(input)
+# print (output.shape)
+
+# writer = SummaryWriter('./log')
+# writer.add_graph(tudui, input)
+# writer.close()
